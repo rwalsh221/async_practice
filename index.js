7e932693606bc71f09bd57161182d92b5d6c89f5
Richard Walsh <richwalsh2@googlemail.com>
2022-01-29 12:53:54 +0000

    add current users list

--- index.js
+++ index.js
@@ -24,23 +24,23 @@ class NewUser {
   }
 }
 
-const newCard = (data) => {
-  const cardHtml = `<div class="userCard">
-  <div class="userCard__heading">
-    <div class="userCard__name">
-      <h2>${data.results[0].name.first} ${data.results[0].name.last}</h2>
-    </div>
-  </div>
-  <div class="userCard__content">
-    <div class="userCard__info">
-      <div class="userCard__dob">${data.results[0].dob.date}</div>
-      <div class="userCard__email">${data.results[0].email}</div>
-    </div>
-  </div>
-  </div>`;
+// const newCard = (data) => {
+//   const cardHtml = `<div class="userCard">
+//   <div class="userCard__heading">
+//     <div class="userCard__name">
+//       <h2>${data.results[0].name.first} ${data.results[0].name.last}</h2>
+//     </div>
+//   </div>
+//   <div class="userCard__content">
+//     <div class="userCard__info">
+//       <div class="userCard__dob">${data.results[0].dob.date}</div>
+//       <div class="userCard__email">${data.results[0].email}</div>
+//     </div>
+//   </div>
+//   </div>`;
 
-  userArr.push(cardHtml);
-};
+//   userArr.push(cardHtml);
+// };
 
 const getAsyncData = async () => {
   try {
@@ -68,17 +68,33 @@ const render = (inputArr) => {
   });
 };
 
+const addUserToList = (user) => {
+  const parent = document.getElementById('userNameList');
+
+  const listItem = document.createElement('li');
+  const paragraph = document.createElement('p');
+
+  const paragraphContent = document.createTextNode(
+    `${user.firstName} ${user.lastName}`
+  );
+
+  paragraph.appendChild(paragraphContent);
+  listItem.appendChild(paragraph);
+
+  parent.insertAdjacentElement('beforeend', listItem);
+};
+
 const newUserCard = async () => {
   try {
     const data = await getAsyncData();
-    // newCard(data);
-    userArr.push(new NewUser(data).generateHtml());
-    console.log(userArr);
+    const user = new NewUser(data);
+    userArr.push(user.generateHtml());
+
+    addUserToList(user);
     render(userArr);
   } catch (err) {
     console.error(err);
   }
 };
 
-// EVENT LISTNER
 document.getElementById('btn__newUser').addEventListener('click', newUserCard);
