c7252975eed10cc2743ddc25a45ad01ac0b90ff8
Richard Walsh <richwalsh2@googlemail.com>
2022-01-29 12:13:12 +0000

    add newUser class

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const userArr = [];
 
+class NewUser {
+  constructor(data) {
+    this.firstName = data.results[0].name.first;
+    this.lastName = data.results[0].name.last;
+    this.dateOfBirth = data.results[0].dob.date;
+    this.email = data.results[0].email;
+  }
+  generateHtml() {
+    return `<div class="userCard">
+      <div class="userCard__heading">
+        <div class="userCard__name">
+          <h2>${this.firstName} ${this.lastName}</h2>
+        </div>
+      </div>
+      <div class="userCard__content">
+        <div class="userCard__info">
+          <div class="userCard__dob">${this.dateOfBirth}</div>
+          <div class="userCard__email">${this.email}</div>
+        </div>
+      </div>
+      </div>`;
+  }
+}
+
 const newCard = (data) => {
   const cardHtml = `<div class="userCard">
   <div class="userCard__heading">
@@ -28,26 +52,33 @@ const getAsyncData = async () => {
   }
 };
 
-const render = () => {
-  const parent = document.getElementById('cardContainer');
-
+const removeChildren = (parent) => {
   while (parent.lastChild) {
     parent.removeChild(parent.lastChild);
   }
-  userArr.forEach((element) => {
+};
+
+const render = (inputArr) => {
+  const parent = document.getElementById('cardContainer');
+
+  removeChildren(parent);
+
+  inputArr.forEach((element) => {
     parent.insertAdjacentHTML('beforeend', element);
   });
 };
 
-const newUser = async () => {
+const newUserCard = async () => {
   try {
     const data = await getAsyncData();
-    newCard(data);
-    render();
+    // newCard(data);
+    userArr.push(new NewUser(data).generateHtml());
+    console.log(userArr);
+    render(userArr);
   } catch (err) {
     console.error(err);
   }
 };
 
 // EVENT LISTNER
-document.getElementById('btn__newUser').addEventListener('click', newUser);
+document.getElementById('btn__newUser').addEventListener('click', newUserCard);
